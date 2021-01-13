d719024299172cd2656fe95a09c97011279ab084
yuwen chang <provence5058@gmail.com>
2021-01-13 18:53:45 +0800

    feat: add user login and register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,5 +5,13 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+
+})
+
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 module.exports = router
