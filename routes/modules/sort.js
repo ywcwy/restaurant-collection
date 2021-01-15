1c949ca37f5cb8bb8587ef24608ed42fd0d1d0c0
yuwen chang <provence5058@gmail.com>
2021-01-15 13:25:01 +0800

    modify index style

--- routes/modules/sort.js
+++ routes/modules/sort.js
@@ -4,32 +4,30 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 handlebars.registerHelper({
-  ifActive: function (sort, target, options) {
+  isActive: function (sort, target, options) {
     if (sort === target) {
-      return options.fn(this);
+      return options.fn(this)
     } else {
-      return options.inverse(this);
+      return options.inverse(this)
     }
   },
-  ifSort: function (item, target, options) {
+  isSort: function (item, target, options) {
     if (item === target) {
-      return options.fn(this);
+      return options.fn(this)
     } else {
-      return options.inverse(this);
+      return options.inverse(this)
     }
   }
 })
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  const item = req.query.item
-  const sort = req.query.sort
+  const item = req.query.item // 排序的項目
+  const sort = req.query.sort // 排序的方式
   Restaurant.find({ userId })
     .lean()
     .sort({ [item]: sort })
-    .then(restaurants => res.render('index', {
-      restaurants, css: 'index.css', sort, item
-    }))
+    .then(restaurants => res.render('index', { restaurants, css: 'index.css', sort, item }))
     .catch(error => console.log(error))
 })
 
