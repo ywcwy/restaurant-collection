81e413f6c88d296ce377b8d8e556717782b85b3c
yuwen chang <provence5058@gmail.com>
2021-01-15 11:15:05 +0800

    modify search feature

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -9,7 +9,10 @@ router.get('/', (req, res) => {
   Restaurant.find({ userId, $or: [{ name: new RegExp(keyword, 'i') }, { category: new RegExp(keyword, 'i') }] })  // 以建構子函式建立正規表達式物件，使用'i'讓文字不區分大小寫
     .lean()
     .then(restaurants => {
-      req.flash('search_msg', `無與${keyword}有關的關鍵字。`)
+      if (!restaurants.length) {
+        const search_msg = `無與${keyword}有關的關鍵字。`
+        return res.render('index', { restaurants, css: 'index.css', search_msg })
+      }
       return res.render('index', { restaurants, css: 'index.css' })
     })
     .catch(error => console.log(error))
